e3a878530c6cc6f99a743f38e1583ca55340f2fb
curist <curist.cyc@gmail.com>
2017-11-16 19:56:06 +0800

    a working version

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  type: 'react-app'
+  type: 'react-app',
+  webpack: {
+    extra: {
+      node: {
+        fs: 'empty'
+      }
+    },
+    rules: {
+      css: {
+        options: {
+          modules: true,
+          localIdentName: '[local]-[hash:base64:10]'
+        }
+      }
+    }
+  }
 }
